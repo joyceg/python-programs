2c49e7395213fae0fd1f5527777d23d77861542d
Joyce <geojoyce934@gmail.com>
2017-01-15 21:09:22 +0530

    printing and simple usage

---
+++ print.py
@@ -0,0 +1,6 @@
+#This is a sample program to print certain statements
+#Good Luck ahead.
+print 'Let me begin this journey.'
+print 'Ic want to learn python.'
+print 'Happy new year 2017.'
+print 'hello', 1+4
---
+++ test.py
@@ -0,0 +1,1 @@
+print 'hello %s there.'% 'hello'
